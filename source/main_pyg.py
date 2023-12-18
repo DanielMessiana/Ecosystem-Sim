08cd05a5de5182444287d16318a24e6c4c01af22
Daniel Messiana <40705754+DanielMessiana@users.noreply.github.com>
2023-12-18 15:02:03 -0500

    Update main_pyg.py

--- source/main_pyg.py
+++ source/main_pyg.py
@@ -47,6 +47,13 @@ class Rabbit:
 		self.target_position = np.array([rand.uniform(0, width), rand.uniform(0, height)])
 		self.age = 0
 		self.energy = 100
+		self.reproduction_cooldown = 0
+		self.max_offspring = 3
+
+		self.resting = False
+		self.resting_cooldown = 0
+		self.max_resting_duration = 500
+		self.resting_duration = 0
 
 	def draw_rabbit(self, position):
 		pygame.draw.circle(rabbit_surface, brown, (int(self.position[0]), int(self.position[1])), self.size)
@@ -54,53 +61,82 @@ class Rabbit:
 	def move(self, food_src):
 		self.last_position = self.position.copy()
 
-		# Check if the rabbit has reached its target position
-		if np.linalg.norm(self.position - self.target_position) < 10:
-			# If reached, set a new random target position
-			self.target_position = np.array([rand.uniform(0, width), rand.uniform(0, height)])
+		near_burrow = any(burrow.is_inside(self.position) for burrow in burrow_src)
 
-		if self.hunger > 70 and np.random.rand() < 0.1:
-			# Wander slowly or sit in place
-			self.speed = 1  # Adjust the speed as needed
-			self.wander_during_break()
-			return
+		if near_burrow:
+			# Increase resting duration even if not already resting
+			self.resting_duration += clock.get_time()
 
-		# Calculate the center and radius of the visual field (circle)
-		visual_field_center = self.position
-		visual_field_radius = 30  # Adjust the radius as needed
+			# If not already resting, start resting
+			if not self.resting and self.resting_cooldown <= 0:
+				self.resting = True
+				self.resting_duration = 0
 
-		# Filter food items within the visual field
-		food_in_visual_field = [food for food in food_src if np.linalg.norm(self.position - np.array([food.x, food.y])) < visual_field_radius]
+			# Check if the rabbit has rested enough, reset speed and cooldown
+			if self.resting_duration >= self.max_resting_duration:
+				self.resting = False
+				self.resting_cooldown = 100  # Cooldown before the rabbit can rest again
+				self.speed = np.random.randint(2, 8)
 
-		if food_in_visual_field:
-			# If there is food in the visual field, move towards the closest one
-			closest_food = min(food_in_visual_field, key=lambda food: np.linalg.norm(self.position - np.array([food.x, food.y])))
-			food_direction = np.array([closest_food.x, closest_food.y]) - self.position
-			food_direction /= np.linalg.norm(food_direction)  # Normalize to unit vector
-			velocity = food_direction * self.speed
-			self.position += velocity
 		else:
-			# If no food in the visual field, move towards the target position
-			target_direction = self.target_position - self.position
-			target_direction /= np.linalg.norm(target_direction)  # Normalize to unit vector
-			velocity = target_direction * self.speed
-			self.position += velocity
+			if self.resting:
 
-		# Constrain Rabbit to screen
-		self.position[0] = np.clip(self.position[0], 0, width)
-		self.position[1] = np.clip(self.position[1], 0, height)
+				self.speed = min(self.speed + 0.1, np.random.randint(2, 8))
+			else:
+				if self.resting_cooldown > 0:
+					self.resting_cooldown -= 1
 
-		self.hunger -= self.hunger_decay_rate
-		self.energy -= 0.1
+				# Check if the rabbit has reached its target position
+				if np.linalg.norm(self.position - self.target_position) < 10:
+					# If reached, set a new random target position
+					self.target_position = np.array([rand.uniform(0, width), rand.uniform(0, height)])
 
-		self.speed = max(2, self.speed - 0.05 * (self.max_hunger - self.hunger))
+				if self.hunger > 70 and np.random.rand() < 0.1:
+					# Wander slowly or sit in place
+					self.speed = 1  # Adjust the speed as needed
+					self.wander_during_break()
+					return
 
-		self.age += 1
+				# Calculate the center and radius of the visual field (circle)
+				visual_field_center = self.position
+				visual_field_radius = 30  # Adjust the radius as needed
 
-		age_death_probability = self.age / 100.0
+				# Filter food items within the visual field
+				food_in_visual_field = [food for food in food_src if np.linalg.norm(self.position - np.array([food.x, food.y])) < visual_field_radius]
 
-		if np.random.rand() < age_death_probability:
-			self.reset
+				if food_in_visual_field:
+					# If there is food in the visual field, move towards the closest one
+					closest_food = min(food_in_visual_field, key=lambda food: np.linalg.norm(self.position - np.array([food.x, food.y])))
+					food_direction = np.array([closest_food.x, closest_food.y]) - self.position
+					food_direction /= np.linalg.norm(food_direction)  # Normalize to unit vector
+					velocity = food_direction * self.speed
+					self.position += velocity
+				else:
+					# If no food in the visual field, move towards the target position
+					target_direction = self.target_position - self.position
+					target_direction /= np.linalg.norm(target_direction)  # Normalize to unit vector
+					velocity = target_direction * self.speed
+					self.position += velocity
+
+				# Constrain Rabbit to screen
+				self.position[0] = np.clip(self.position[0], 0, width)
+				self.position[1] = np.clip(self.position[1], 0, height)
+
+				self.hunger -= self.hunger_decay_rate
+				self.energy -= 0.1
+
+				self.speed = max(2, self.speed - 0.05 * (self.max_hunger - self.hunger))
+
+				self.age += 1
+
+				age_death_probability = self.age / 100.0
+
+				if np.random.rand() < age_death_probability:
+					self.reset
+
+				self.reproduction_cooldown = max(0, self.reproduction_cooldown - 1)
+
+
 
 	def wander_during_break(self):
 		if np.random.rand() < 0.5:
@@ -108,7 +144,7 @@ class Rabbit:
 			random_direction /= np.linalg.norm(random_direction)  # Normalize to unit vector
 			velocity = random_direction * self.speed
 			self.position += velocity
-			self.total_reward += 1
+			self.total_reward += 3
 		else:
 			pass
 
@@ -123,12 +159,15 @@ class Rabbit:
 
 	def reproduce(self):
 		# Check conditions for reproduction (e.g., age, energy level)
-		if self.age >= 20 and self.energy >= 50:
+		if self.age >= 20 and self.energy >= 50 and self.reproduction_cooldown <= 0:
 			# Create a new rabbit with inherited traits
 			new_rabbit = Rabbit(self.speed, self.size, self.position[0], self.position[1])
 			new_rabbit.nn.load_state_dict(self.nn.state_dict())  # Inherit neural network weights
 			new_rabbit.max_hunger = self.max_hunger  # Inherit max_hunger
 			# You can add more traits to inherit
+
+			self.reproduction_cooldown = 100
+			self.max_offspring -= 1
 
 			return new_rabbit
 		else:
@@ -193,10 +232,10 @@ class Food:
 		self.hp -= self.decay_rate
 
 def create_food(food_src):
-	if np.random.randint(0, 100) > 96:
+	if np.random.randint(0, 100) > 97:
 		n = np.random.randint(0, 10)
 		while n > 0:
-			f = Food(np.random.randint(10, 30))
+			f = Food(np.random.randint(1, 30))
 			food_src.append(f)
 			n -= 1
 
@@ -205,6 +244,20 @@ def update_food(food_src):
 		food.update()
 
 	return [food for food in food_src if food.hp > 0]
+
+class Burrow:
+
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+		self.radius = 40
+
+	def draw_burrow(self):
+		pygame.draw.circle(burrow_surface, light_brown, (int(self.x), int(self.y)), self.radius)
+
+	def is_inside(self, position):
+		distance = np.linalg.norm(np.array([self.x, self.y]) - position)
+		return distance < self.radius
 
 pygame.init()
 width, height = 1900, 1150
@@ -216,17 +269,20 @@ main = True
 
 white = (255, 255, 255)
 brown = (150, 75, 0)
+light_brown = (194, 164, 132, 140)
 green = (0, 255, 0)
 
 food_surface = pygame.Surface((width, height), pygame.SRCALPHA)
+burrow_surface = pygame.Surface((width, height), pygame.SRCALPHA)
 rabbit_surface = pygame.Surface((width, height), pygame.SRCALPHA)
 
 def run_simulation():
 	# Create a set of rabbits and food sources for each simulation
 	speed = np.random.randint(2, 8)
-	size = np.random.randint(20, 30)
+	size = np.random.randint(5, 20)
 
-	food_src = [Food(np.random.randint(10, 30)) for _ in range(10)]
+	food_src = [Food(np.random.randint(10, 20)) for _ in range(10)]
+	#burrow_src = [Burrow(np.random.randint(0, width), np.random.randint(0, height)) for _ in range(np.random.randint(2,6))]
 	rpop = [Rabbit(speed, size) for _ in range(20)]
 
 	num_epochs = 1
@@ -264,11 +320,12 @@ def run_simulation():
 			rabbit.optimizer.step()
 
 		food_src = update_food(food_src)
+		move_rabbits(rpop, food_src, burrow_src)
 
 	return rpop, food_src
 
 
-def move_rabbits(rpop, food_src):
+def move_rabbits(rpop, food_src, burrow_src):
 	if not food_src:
 		return
 
@@ -276,23 +333,37 @@ def move_rabbits(rpop, food_src):
 	food_positions = np.array([[food.x, food.y] for food in food_src])
 
 	for rabbit in rpop:
-		# Calculate distances, directions, and velocities in a vectorized manner
-		distances = np.linalg.norm(rabbit.position - food_positions, axis=1)
+		# Check if the rabbit is close to any burrow
+		near_burrow = any(burrow.is_inside(rabbit.position) for burrow in burrow_src)
 
-		# Find the closest food
-		closest_food_index = np.argmin(distances)
-		closest_food = food_positions[closest_food_index]
+		if near_burrow:
+			# If near a burrow, the rabbit can rest
+			rabbit.wander_during_break()
+		else:
+			# If not near a burrow, continue normal behavior
+			distances = np.linalg.norm(rabbit.position - food_positions, axis=1)
+			closest_food_index = np.argmin(distances)
+			closest_food = food_positions[closest_food_index]
+			food_direction = closest_food - rabbit.position
+			food_direction /= np.linalg.norm(food_direction)
+			velocity = food_direction * rabbit.speed
+			rabbit.position += velocity
 
-		food_direction = closest_food - rabbit.position
-		food_direction /= np.linalg.norm(food_direction)
-		velocity = food_direction * rabbit.speed
+			# Constrain rabbits to the screen
+			rabbit.position[0] = np.clip(rabbit.position[0], 0, width)
+			rabbit.position[1] = np.clip(rabbit.position[1], 0, height)
 
-		# Update rabbit position
-		rabbit.position += velocity
+			rabbit.hunger -= rabbit.hunger_decay_rate
+			rabbit.energy -= 0.1
 
-		# Constrain rabbits to the screen
-		rabbit.position[0] = np.clip(rabbit.position[0], 0, width)
-		rabbit.position[1] = np.clip(rabbit.position[1], 0, height)
+			rabbit.speed = max(2, rabbit.speed - 0.05 * (rabbit.max_hunger - rabbit.hunger))
+
+			rabbit.age += 1
+
+			age_death_probability = rabbit.age / 100.0
+
+			if np.random.rand() < age_death_probability:
+				rabbit.reset()
 
 
 # Run multiple simulations
@@ -301,11 +372,22 @@ all_rabbit_populations = []
 all_food_sources = []
 
 food_src = []
+burrow_src = []
+
+def create_burrow(burrow_src):
+	burrow_src.clear()
+	n = np.random.randint(2, 6)
+	while n > 0:
+		b = Burrow(np.random.randint(0, width), np.random.randint(0, height))
+		burrow_src.append(b)
+		n -= 1
 
 for simulation in range(num_simulations):
 	rpop, food_src = run_simulation()
 	all_rabbit_populations.append(rpop)
 	all_food_sources.append(food_src)
+
+	create_burrow(burrow_src)
 
 # Main game loop
 current_simulation = 0
@@ -338,9 +420,14 @@ while main:
 		# Blit the food surface onto the screen
 		screen_buffer.blit(food_surface, (0, 0))
 
+		burrow_surface.fill((0, 0, 0, 0))
+		for burrow in burrow_src:
+			burrow.draw_burrow()
+		screen_buffer.blit(burrow_surface, (0, 0))
+
 		# Draw rabbit surfaces for the current simulation
 		rpop = all_rabbit_populations[current_simulation]
-		move_rabbits(rpop, food_src)
+		move_rabbits(rpop, food_src, burrow_src)
 
 		for rabbit in rpop:
 			new_rabbit = rabbit.reproduce()
@@ -364,6 +451,7 @@ while main:
 				main = False
 
 		screen.blit(food_surface, (0, 0))
+		screen.blit(burrow_surface, (0, 0))
 		screen.blit(rabbit_surface, (0, 0))
 
 		pygame.display.flip()
